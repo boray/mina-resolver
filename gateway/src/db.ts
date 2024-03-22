d8104ade649a58e75770839d99d02770b8c5bdcc
boray <bsaygilier33@gmail.com>
2024-03-22 21:26:16 +0300

    prettify gateway

--- gateway/src/db.ts
+++ gateway/src/db.ts
@@ -1,10 +1,6 @@
 import { Database } from './server';
 import { EMPTY_CONTENT_HASH, ETH_COIN_TYPE, ZERO_ADDRESS } from './constants';
 
-//const kvNamespace = process.env.NAMESPACE;
-//const kvAccountId = process.env.ACCOUNT_ID;
-//const kvToken = process.env.TOKEN;
-
 interface NameData {
   addresses?: { [coinType: number]: string };
   text?: { [key: string]: string };
@@ -22,7 +18,7 @@ export const database: Database = {
       const addr = nameData?.addresses?.[coinType] || ZERO_ADDRESS;
       return { addr, ttl: 1000 };
     } catch (error) {
-      console.error('Error resolving addr', error);
+      console.error('Error resolving address', error);
       return { addr: ZERO_ADDRESS, ttl: 1000 };
     }
   },
@@ -33,7 +29,7 @@ export const database: Database = {
       const value = nameData?.text?.[key] || '';
       return { value, ttl: 1000 };
     } catch (error) {
-      console.error('Error resolving addr', error);
+      console.error('Error resolving address', error);
       return { value: '', ttl: 1000 };
     }
   },
@@ -46,8 +42,8 @@ async function fetchOffchainName(name: string): Promise<NameData> {
   try {
     let bytes = new TextEncoder().encode(name);
     let length = bytes.length.toString();
-    let chars = bytes.join().split(',')    
-  const response = await fetch('http://127.0.0.1:8080/graphql', {
+    let chars = bytes.join().split(',');   
+    const response = await fetch('http://127.0.0.1:8080/graphql', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
@@ -81,7 +77,7 @@ scheme.addresses[12586] = mina_address;
 
     return scheme as NameData;
   } catch (err) {
-    console.error('Error fetching offchain name', err);
+    console.error('Error fetching name from Mina', err);
     return {};
   }
 }
